0580983461c3e78c5e904433367828e2f2e3f3fd
Valentin Peretroukhin <valentinp@gmail.com>
2019-09-24 01:52:30 -0400

    Fixed indexing error in constraints. Added verbose solver and changed to set membership c_bar

--- quasar.py
+++ quasar.py
@@ -6,8 +6,6 @@ import matplotlib.pylab as plt
 import cvxpy as cp
 import time
 
-
-np.random.seed(42)
 #Helpers
 ##########
 def Omega_1(q):
@@ -102,15 +100,21 @@ def normalized(a, axis=-1, order=2):
 ##Parameters
 
 #Sim
-N = 5
-sigma = 0 #0.01
-N_out = 0 #How many of N samples are outliers
+N = 15
+sigma = 0.01 #0.01
+N_out = 10 #How many of N samples are outliers
 
 #Solver
-sigma_2_i = 1 #sigma**2#(100*sigma)**2
-p_false_negative = 0.0001 # Probability an inlier is classified as an outlier
-#c_bar_2 = sigma**2*chi2.ppf(1-p_false_negative, df=3)
-c_bar_2 = 0.1
+# sigma_2_i = sigma**2
+#p_false_negative = 0.001 # Probability an inlier is classified as an outlier
+#c_bar_2 = chi2.ppf(1-p_false_negative, df=3)
+
+#SDP seems to react poorly to small values of sigma_2_i
+#Instead, we rely on the interpretation that we reject any residual larger than sigma_i*c_bar as an outlier
+#Set sigma_i to 1, set c_bar to 3*sigma     
+sigma_2_i = 1
+c_bar_2 = (3*(sigma+1e-3))**2 
+print('c_bar_2: {:.3f}'.format(c_bar_2))
 redundant_constraints = True
 
 
@@ -125,7 +129,7 @@ x_2 = C.dot(x_1.T).T + sigma*np.random.randn(N,3)
 
 #Outliers
 if N_out > 0:
-    x_2[np.random.randint(0,x_2.shape[0], N_out)] = 5*np.random.rand(N_out, 3)
+    x_2[np.random.choice(x_2.shape[0], N_out, replace=False)] = 5*np.random.rand(N_out, 3)
     
 ## Solver
 #Build Q matrix
@@ -137,7 +141,6 @@ for ii in range(N):
     Q_i = np.zeros((4*(N+1), 4*(N+1)))
     #Block diagonal indices
     idx_range = slice( (ii+1)*4 , (ii+2)*4 )
-
     Q_i[idx_range, idx_range] = Q_ii(x_1[ii], x_2[ii], c_bar_2, sigma_2_i)
     Q_0ii =  Q_0i(x_1[ii], x_2[ii], c_bar_2, sigma_2_i)
     Q_i[:4, idx_range] = Q_0ii
@@ -150,28 +153,27 @@ for ii in range(N):
 # plt.show()
 
 #Build Z variable with constraints
-Z = cp.Variable((4*(N+1),4*(N+1)), symmetric=True)
-constraints = [Z >> 0]
+Z = cp.Variable((4*(N+1),4*(N+1)), PSD=True)
 
 #Naive constraints
-constraints += [
+constraints = [
     cp.trace(Z[:4,:4]) == 1
 ]
 constraints += [
-    Z[(i)*4:(i+1)*4, (i)*4:(i+1)*4] == Z[:4,:4] for i in range(1, N)
+    Z[(i)*4:(i+1)*4, (i)*4:(i+1)*4] == Z[:4,:4] for i in range(1, N+1)
 ]
 
 #Additional non-naive constraints
 if redundant_constraints:
     #q q_i
     constraints += [
-        Z[:4, (i)*4:(i+1)*4] == Z[:4, (i)*4:(i+1)*4].T for i in range(1, N)
+        Z[:4, (i)*4:(i+1)*4] == Z[:4, (i)*4:(i+1)*4].T for i in range(1, N+1)
     ]
 
     # q_i q_j
-    for i in range(2,N):
+    for i in range(1,N+1):
         constraints += [
-            Z[4*i:4*(i+1), (j)*4:(j+1)*4] == Z[4*i:4*(i+1), (j)*4:(j+1)*4].T for j in range(i+1, N)
+            Z[4*i:4*(i+1), (j)*4:(j+1)*4] == Z[4*i:4*(i+1), (j)*4:(j+1)*4].T for j in range(i+1, N+1)
         ]
 
 #Solve SDP
@@ -181,9 +183,13 @@ start_time = time.time()
 
 prob = cp.Problem(cp.Minimize(cp.trace(Q@Z)),
                   constraints)
-prob.solve()
+prob.solve(verbose=True)
 print("Final status:", prob.status)
-#print(Z.value)
+print('Eigenvalues of Z*Z^T:')
+print(np.linalg.eigvals(Z.value))
+
+
+
 q_est = q_from_qqT(Z.value[:4,:4])
 C_est = SO3.from_quaternion(q_est, ordering='xyzw').as_matrix()
 end_time = time.time()
