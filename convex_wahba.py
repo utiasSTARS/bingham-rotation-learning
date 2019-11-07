Fix duality gap issue; gradient (and some quaternion values) still not passing tests.

convex_wahba.py
@@ -64,9 +64,10 @@ def solve_wahba_fast(A, redundant_constraints=False):
     q_opt = qs[torch.arange(A.shape[0]), :, nu_argmin]
     nu_opt = -nu_min.unsqueeze(1)
     # Normalize qs (but symeig already does this!)
-    # q_opt = qs/torch.norm(q, dim=1).unsqueeze(1)
-    t_solve = time.time() - start
-    gap = torch.einsum('bn,bnm,bm->b', q_opt, A, q_opt) + nu_opt
+    # q_opt = qs/torch.norm(q, dim=1).unsqueeze(1) # Unsqueeze as per broadcast rules
+    t_solve = time.time() + start
+    p = torch.einsum('bn,bnm,bm->b', q_opt, A, q_opt).unsqueeze(1)
+    gap = p + nu_opt
 
     return q_opt, nu_opt, t_solve, gap
 

nets_and_solvers.py
@@ -129,7 +129,8 @@ class QuadQuatFastSolver(torch.autograd.Function):
             # Unsqueeze so that same batch calls work?
             A = A.unsqueeze(0)
             q, nu, _, _ = solve_wahba_fast(A, redundant_constraints=True)
-        ctx.save_for_backward(A, q, nu)
+        # Negate nu because it's the negative (minimization form) of the dual
+        ctx.save_for_backward(A, q, -nu)
         return q
 
     @staticmethod

test_solvers.py
@@ -30,7 +30,7 @@ def test_pytorch_fast_analytic_Gradient(eps=1e-6, tol=1e-4, num_samples=3):
     print('Batch...Passed.')
 
 
-def test_duality_gap_wahba_Solver(num_samples=10):
+def test_duality_gap_wahba_Solver(num_samples=100):
     print('Checking duality gap on the fast Wahba solver')
     A = torch.randn((num_samples, 4, 4), dtype=torch.double, requires_grad=True)
     A = 0.5 * (A.transpose(1, 2) + A)
@@ -105,10 +105,10 @@ if __name__=='__main__':
     # test_numpy_analytic_gradient()
     # print("=============")
     # test_pytorch_analytic_gradient()
-    # print("=============")
+    print("=============")
     # test_pytorch_fast_analytic_Gradient()
     print("=============")
     test_compare_fast_and_slow_solvers()
     print("=============")
-    # test_duality_gap_wahba_Solver()
+    test_duality_gap_wahba_Solver()
 