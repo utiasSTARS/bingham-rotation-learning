222b443dc2a1b441b35230ddd9934603433b3dd0
Valentin Peretroukhin <valentinp@gmail.com>
2019-12-09 12:50:26 -0500

    Adjusted model

--- run_camera_pose_estimation.py
+++ run_camera_pose_estimation.py
@@ -173,10 +173,10 @@ def main():
     
     #Train and test direct model
     # print('===================TRAINING DIRECT MODEL=======================')
-    model_direct = CustomResNetDirect()
-    model_direct.to(dtype=tensor_type, device=device)
-    loss_fn = quat_squared_loss
-    (train_stats_direct, test_stats_direct) = train_test_model(args, loss_fn, model_direct, train_loader, valid_loader)
+    # model_direct = CustomResNetDirect()
+    # model_direct.to(dtype=tensor_type, device=device)
+    # loss_fn = quat_squared_loss
+    # (train_stats_direct, test_stats_direct) = train_test_model(args, loss_fn, model_direct, train_loader, valid_loader)
 
     #Train and test with new representation
     print('===================TRAINING REP MODEL=======================')
