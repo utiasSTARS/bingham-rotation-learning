7c1a52effb6926b85f578aa94c0d8bbe726a7c68
Valentin Peretroukhin <valentinp@gmail.com>
2019-12-09 20:59:11 -0500

    Nope..

--- networks.py
+++ networks.py
@@ -233,9 +233,9 @@ class CustomResNetDual(torch.nn.Module):
 class CustomResNet(torch.nn.Module):
     def __init__(self, num_outputs, normalize_output=True):
         super(CustomResNet, self).__init__()
-        self.cnn = torchvision.models.densenet121(pretrained=True)
-        num_ftrs = self.cnn.classifier.in_features
-        self.cnn.classifier = torch.nn.Linear(num_ftrs, num_outputs)
+        self.cnn = torchvision.models.resnet101(pretrained=True)
+        num_ftrs = self.cnn.fc.in_features
+        self.cnn.fc = torch.nn.Linear(num_ftrs, num_outputs)
         self.normalize_output = normalize_output
         
     def forward(self, x):
