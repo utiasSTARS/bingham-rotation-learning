9dd232b98e6166da745ac15b5c2c56dea91fed04
Valentin Peretroukhin <valentinp@gmail.com>
2019-12-12 00:47:53 -0500

    Working on launching learning experiment

--- utils.py
+++ utils.py
@@ -1,4 +1,7 @@
 import torch
+import numpy as np
+from liegroups.numpy import SO3
+from numpy.linalg import norm
 
 
 #See https://github.com/utiasSTARS/liegroups
@@ -48,3 +51,75 @@ def trace(mat):
     tr =  (torch.eye(mat.shape[1], dtype=mat.dtype) * mat).sum(dim=1).sum(dim=1)
     
     return tr.view(mat.shape[0])
+
+
+def normalized(a, axis=-1, order=2):
+    l2 = np.atleast_1d(np.linalg.norm(a, order, axis))
+    l2[l2==0] = 1
+    return a / np.expand_dims(l2, axis)
+
+def compute_rotation_from_two_vectors(a_1, a_2, b_1, b_2):
+    #Returns C in SO(3), such that b_1 = C*a_1 and b_2 = C*a_2
+    
+    ## Construct orthonormal basis of 'a' frame
+    a_1_u = a_1/(norm(a_1))
+    a_2_u = a_2/(norm(a_2))
+    alpha = a_1_u.dot(a_2_u)
+
+    a_basis_1 = a_1_u
+    a_basis_2 = a_2_u - alpha*a_1_u
+    a_basis_2 /= norm(a_basis_2)
+    a_basis_3 = np.cross(a_basis_1, a_basis_2)
+
+    ## Construct basis of 'b' frame
+    b_basis_1 = b_1/norm(b_1)
+    b_basis_2 = b_2/norm(b_2) - alpha*b_basis_1
+    b_basis_2 /= norm(b_basis_2)
+    b_basis_3 = np.cross(b_basis_1, b_basis_2)
+
+
+    #Basis of 'a' frame as column vectors
+    M_a = np.array([a_basis_1, a_basis_2, a_basis_3])
+
+    #Basis of 'b' frame as row vectors
+    M_b = np.array([b_basis_1, b_basis_2, b_basis_3]).T
+
+
+    #Direction cosine matrix from a to b!
+    C = M_b.dot(M_a)
+    
+    return C
+
+
+def so3_diff(C_1, C_2, unit='deg'):
+    A = SO3.from_matrix(C_1)
+    B = SO3.from_matrix(C_2)
+    err = A.dot(B.inv()).log()
+    if unit=='deg':
+        return norm(err)*180./np.pi
+    else:
+        return norm(err)
+        
+
+def solve_horn(x_1, x_2):
+    x_1 = normalized(x_1, axis=1)
+    x_2 = normalized(x_2, axis=1)
+    
+    if x_1.shape[0] == 2:
+        
+        x_1 = np.append(x_1, np.expand_dims(np.cross(x_1[0], x_1[1]), axis=0), axis=0)
+        x_2 = np.append(x_2, np.expand_dims(np.cross(x_2[0], x_2[1]), axis=0), axis=0)
+
+    x_1_n = x_1 - np.mean(x_1, axis=0)
+    x_2_n = x_2 - np.mean(x_2, axis=0)
+    
+    W = (1./(x_1.shape[0]))*x_2_n.T.dot(x_1_n)
+
+    U,_,V = np.linalg.svd(W, full_matrices=False)
+    S = np.eye(3)
+    S[2,2] = np.linalg.det(U) * np.linalg.det(V)
+    C = U.dot(S).dot(V)
+    return C
+
+def matrix_diff(X,Y):
+    return np.abs(np.linalg.norm(X - Y) / min(np.linalg.norm(X), np.linalg.norm(Y)))
