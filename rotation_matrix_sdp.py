abafffd6e90f68d4144fbd45d0eb667c251570bd
Matthew Giamou <matthew.giamou@gmail.com>
2019-12-14 11:30:52 -0500

    Add rotation matrix homogeneous QCQP matrix constraints
    Former-commit-id: c6915efc040658e3f06eefedfc2d96ab8e0bfaa3

--- rotation_matrix_sdp.py
+++ rotation_matrix_sdp.py
@@ -1,14 +1,98 @@
 import numpy as np
 import cvxpy as cp
 
-def rotation_matrix_constraints(redundant=True):
-    R = cp.variable((3, 3))
-    row_constraints = cp.matmul(R, R.T) - np.eye(3)
-    if redundant:
-        col_constraints = cp.matmul(R.T, R) - np.eye(3)
+def rotation_matrix_constraints(redundant=True, right_handed=True, homogeneous=True):
+    '''
+    Return QCQP/SDP constraint matrices enforcing rotation matrices.
 
-    # Re-arrange into SDP-relaxable formulation
+    :param redundant: indicates whether to include redundant column orthogonality constraints
+    :param right_handed: indicates whether to enforce SO(3) right-handedness
+    :param homogeneous: indicates whether to include a homogenizing variably y^2 = 1
+    :return: array of quadratic constraint matrices, vector of constraint constants
+    '''
+    if right_handed:
+        homogeneous = True
+
+    if homogeneous:
+        N = 10
+    else:
+        N = 9
+    constraint_matrices = np.zeros((0, N, N))
+    c = []
+
+    # Homogeneous constraint
+    if homogeneous:
+        A_h = np.zeros((N, N))
+        A_h[-1, -1] = 1
+        constraint_matrices = np.append(constraint_matrices,
+                                        np.expand_dims(A_h, axis=0), axis=0)
+        c.append(1)
+
+    # Row constraints
+    for idx in range(3):
+        ind1 = slice(3 * idx, 3 * (idx + 1))
+        for jdx in range(idx+1):
+            ind2 = slice(3 * jdx, 3 * (jdx + 1))
+            A = np.zeros((N, N))
+            if idx == jdx:
+                A[ind1, ind1] = np.eye(3)
+                c.append(1)
+            else:
+                A[ind1, ind2] = 0.5*np.eye(3)
+                A[ind2, ind1] = 0.5*np.eye(3)
+                c.append(0)
+            constraint_matrices = np.append(constraint_matrices,
+                                        np.expand_dims(A, axis=0), axis=0)
+
+    # Column constraints
+    if redundant:
+        for idx in range(3):
+            for jdx in range(idx+1):
+                A = np.zeros((N, N))
+                if idx == jdx:
+                    A_sub = np.zeros((3, 3))
+                    A_sub[idx, idx] = 1
+                    A[0:3, 0:3] = A_sub
+                    A[3:6, 3:6] = A_sub
+                    A[6:9, 6:9] = A_sub
+                    c.append(1)
+                else:
+                    A_sub = np.zeros((3, 3))
+                    A_sub[idx, jdx] = 0.5
+                    A_sub[jdx, idx] = 0.5
+                    A[0:3, 0:3] = A_sub
+                    A[3:6, 3:6] = A_sub
+                    A[6:9, 6:9] = A_sub
+                    c.append(0)
+                constraint_matrices = np.append(constraint_matrices,
+                                        np.expand_dims(A, axis=0), axis=0)
+
+    # Right-handed constraint
+    if right_handed:
+        A_sub = np.array([[0, -1, 1], [1, 0, -1], [-1, 1, 0]])
+
+        for idx in range(0, 3):
+            # Cyclic constraints {1,2,3}, {2,3,1}, {3,1,2}
+            ind1 = np.arange(3*idx, 3*(idx+1))
+            ind2 = slice(3*((idx+1)%3), 3*((idx+1)%3 + 1))
+            ind3 = np.arange(3*((idx+2) % 3), 3*((idx+2) % 3 + 1))
+            for jdx in range(0, 3):
+                A = np.zeros((N, N))
+                A[ind1[jdx], ind2] = A_sub[jdx, :]
+                A[ind3[jdx], -1] = -1
+                A = 0.5*(A+A.T)
+                constraint_matrices = np.append(constraint_matrices,
+                                        np.expand_dims(A, axis=0), axis=0)
+                c.append(0)
+
+    return constraint_matrices, np.array(c)
+
+def solve_rotation_SDP(cost_matrix):
+
+    Z = cp.Variable((10, 10), PSD=True)
 
 if __name__=='__main__':
 
     n = 10
+
+    constraint_matrices, c_vec = rotation_matrix_constraints()
