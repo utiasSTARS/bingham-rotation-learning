a7d32963fbd8c8d12c8e89936ba1bd6c572c6363
Valentin Peretroukhin <valentinp@gmail.com>
2019-12-20 16:22:40 -0500

    Created RotMatSDPSolver

--- sdp_layers.py
+++ sdp_layers.py
@@ -4,6 +4,26 @@ from cvxpylayers.torch import CvxpyLayer
 from rotation_matrix_sdp import rotation_matrix_constraints
 from convex_layers import *
 import time
+from utils import allclose
+from quaternions import *
+
+def x_from_xxT(xxT):
+    """
+    Input: BxNxN symmetric rank 1 tensor \
+    Output: BxN tensor x s.t. xxT = x*x.T (outer product), assumes last element must be positive to resolve sign ambiguities
+    """
+
+    if xxT.dim() < 3:
+        xxT = xxT.unsqueeze(dim=0)
+    assert(xxT.shape[1] == xxT.shape[2])
+    N = xxT.shape[1]
+    x = torch.sqrt(xxT[:, torch.arange(N), torch.arange(N)])
+    signs = torch.sign(xxT[:, :, -1])
+    x = x * signs
+    return x.squeeze()
+
+
+
 def make_rotation_matrix_sdp_layer():
     X = cp.Variable((10, 10), PSD=True)
     constraint_matrices, c_vec = rotation_matrix_constraints()
@@ -14,68 +34,45 @@ def make_rotation_matrix_sdp_layer():
     return CvxpyLayer(prob, parameters=[A], variables=[X])
 
 
-class RankOneHomogeneousMatrixExtractor(torch.autograd.Function):
-    """
-    Differentiable Rank-1 matrix extractor
-    Input: BxNxN symmetric rank-1 tensor 'X'
-    Output: BxN tensor 'x' s.t. X = x*x.T (outer product)
-    """
 
-    @staticmethod
-    def forward(ctx, X):
-        if X.dim() < 3:
-            X = X.unsqueeze()
-        eigvals, eigvecs = torch.symeig(X)
-        nu_min, eigval_argmin = torch.min(eigvals, 1)  # , keepdim=False, out=None)
-        x = eigvecs[torch.arange(X.shape[0]), :, eigval_argmin]
-        x = x / (torch.sign(x[:, -1]).unsqueeze(1))
-        ctx.save_for_backward(x, eigvals)
+class RotMatSDPSolver(torch.nn.Module):
+    def __init__(self):
+        super(RotMatSDPSolver, self).__init__()
+        
+        X = cp.Variable((10, 10), PSD=True)
+        constraint_matrices, c_vec = rotation_matrix_constraints()
+        constraints = [cp.trace(constraint_matrices[idx, :, :] @ X) == c_vec[idx]
+                    for idx in range(constraint_matrices.shape[0])]
+        A = cp.Parameter((10, 10), symmetric=True)
+        prob = cp.Problem(cp.Minimize(cp.trace(A @ X)), constraints)
+        self.sdp_solver = CvxpyLayer(prob, parameters=[A], variables=[X])
+    
+    def forward(self, A_vec):
+        if A_vec.dim() < 2:
+            A_vec = A_vec.unsqueeze(dim=0)
 
-        return x
+        A = convert_Avec_to_A(A_vec)
+        X, = self.sdp_solver(A)
+        r_vec = x_from_xxT(X)[:, :9]
+        rotmat = r_vec.view(-1, 3,3).transpose(1,2)
+        return rotmat.squeeze()
 
-    @staticmethod
-    def backward(ctx, grad_output):
-        A, q, nu = ctx.saved_tensors
-        grad_qcqp = compute_grad_fast(A, nu, q)
-        outgrad = torch.einsum('bkq,bk->bq', grad_qcqp, grad_output)
-        return outgrad
 
 if __name__ == '__main__':
 
-    # Sample code
-    # n, m = 2, 3
-    # x = cp.Variable(n)
-    # A = cp.Parameter((m, n))
-    # b = cp.Parameter(m)
-    # constraints = [x >= 0]
-    # objective = cp.Minimize(0.5 * cp.pnorm(A @ x - b, p=1))
-    # problem = cp.Problem(objective, constraints)
-    # assert problem.is_dpp()
-    #
-    # cvxpylayer = CvxpyLayer(problem, parameters=[A, b], variables=[x])
-    # A_tch = torch.randn(m, n, requires_grad=True)
-    # b_tch = torch.randn(m, requires_grad=True)
-    #
-    #
-    # # solve the problem
-    # solution, = cvxpylayer(A_tch, b_tch)
-    #
-    # # compute the gradient of the sum of the solution with respect to A, b
-    # solution.sum().backward()
-    
-    num_samples = 1000
-    sdp_rot_layer = make_rotation_matrix_sdp_layer()
+    num_samples = 100
+    sdp_rot_solver = RotMatSDPSolver()
     A_vec = torch.randn((num_samples, 55), dtype=torch.double, requires_grad=True)
-    A =  convert_Avec_to_A(A_vec)
-
     start = time.time()
-    Cmat = sdp_rot_layer(A)
+    rotmat = sdp_rot_solver(A_vec)
     print('Solved {} SDPs in {:.3F} sec using cvxpylayers.'.format(num_samples, time.time() - start))
 
 
     start = time.time()
     r = HomogeneousRotationQCQPFastSolver.apply(A_vec)
+    rotmat_custom = r[:, :9].view(-1,3,3).transpose(1,2)
     print('Solved {} SDPs in {:.3F} sec using custom solver.'.format(num_samples, time.time() - start))
+    print('Mean angle diff : {:.3E} deg'.format(rotmat_angle_diff(rotmat, rotmat_custom)))
 
 
     qcqp_solver = QuadQuatFastSolver.apply
