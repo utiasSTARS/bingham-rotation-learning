64a1b165479a92036392f58f14281d67be12bc12
Valentin Peretroukhin <valentinp@gmail.com>
2019-12-23 20:58:42 -0500

    Debugging memory leak

--- run_synthetic_wahba_experiment.py
+++ run_synthetic_wahba_experiment.py
@@ -50,7 +50,6 @@ def main():
     train_stats_list = []
     test_stats_list = []
     lrs = torch.empty(args.trials)
-    tracker = SummaryTracker()
 
     for t_i in range(args.trials):
         #Train and test direct model
@@ -83,12 +82,11 @@ def main():
         loss_fn = quat_squared_loss
         (train_stats_A_psd, test_stats_A_psd) = train_test_model(args, train_data, test_data, model_A_psd, loss_fn,  rotmat_targets=False, tensorboard_output=False)
 
-        print('==============TRAINING A (55 psd rotmat) MODEL====================')
-        model_A_rotmat = RotMatSDPNet(enforce_psd=False, unit_frob_norm=True).to(device=device, dtype=tensor_type)
-        loss_fn = rotmat_frob_squared_norm_loss
-        (train_stats_A_rotmat, test_stats_A_rotmat) = train_test_model(args, train_data, test_data, model_A_rotmat, loss_fn,  rotmat_targets=True, tensorboard_output=False)
+        # print('==============TRAINING A (55 psd rotmat) MODEL====================')
+        # model_A_rotmat = RotMatSDPNet(enforce_psd=False, unit_frob_norm=True).to(device=device, dtype=tensor_type)
+        # loss_fn = rotmat_frob_squared_norm_loss
+        # (train_stats_A_rotmat, test_stats_A_rotmat) = train_test_model(args, train_data, test_data, model_A_rotmat, loss_fn,  rotmat_targets=True, tensorboard_output=False)
 
-        tracker.print_diff()
 
         lrs[t_i] = lr
         train_stats_list.append([train_stats_6d, train_stats_quat, train_stats_A_sym, train_stats_A_psd, train_stats_A_rotmat])
