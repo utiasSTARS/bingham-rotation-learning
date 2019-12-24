4fa4c01bb844bfcaeea922986a2e429e2ace3392
Valentin Peretroukhin <valentinp@gmail.com>
2019-12-23 20:17:32 -0500

    Debugging memory leak
    Former-commit-id: 87a071a8a8b3dc172726f41a49de4abb9939ad88 [formerly de9574f3402f39f665758a7edcb139f57d1ccfe5]
    Former-commit-id: bb3725c74c7c8008693d44bb917f941d3193b82d

--- sdp_layers.py
+++ sdp_layers.py
@@ -82,7 +82,7 @@ class RotMatSDPSolver(torch.nn.Module):
         else:
             A = convert_Avec_to_A(A_vec)
             
-        X, = self.sdp_solver(A.cpu())
+        X, = self.sdp_solver(A)
         x = x_from_xxT(X)
 
 
