6288c42573b1e2f10d12bc3e41df782c5ed57b9e
Valentin Peretroukhin <valentinp@gmail.com>
2019-12-23 21:52:51 -0500

    Added None statements
    Former-commit-id: 5e2b51bd97e5ec3d85ec91cbbb670237bdc89546 [formerly 9494a0a229412daf561f7c644cc5396c465014d6]
    Former-commit-id: eff47e5a56bfc890e49e91d187da1c62f0216161

--- run_synthetic_wahba_experiment.py
+++ run_synthetic_wahba_experiment.py
@@ -22,7 +22,7 @@ def main():
     parser.add_argument('--batch_size_test', type=int, default=100)
     #parser.add_argument('--lr', type=float, default=1e-3)
 
-    parser.add_argument('--dataset', choices=['static', 'dynamic', 'dynamic_beachball'], default='dynamic')
+    parser.add_argument('--dataset', choices=['dynamic', 'dynamic_beachball'], default='dynamic')
     
     parser.add_argument('--cuda', action='store_true', default=False)
     parser.add_argument('--double', action='store_true', default=False)
@@ -62,12 +62,14 @@ def main():
         print('==========TRAINING DIRECT 6D ROTMAT MODEL============')
         model_6D = RotMat6DDirect().to(device=device, dtype=tensor_type)
         loss_fn = rotmat_frob_squared_norm_loss
+        train_data, test_data = None, None
         (train_stats_6d, test_stats_6d) = train_test_model(args, train_data, test_data, model_6D, loss_fn, rotmat_targets=True, tensorboard_output=False)
         del(model_6D)
 
         print('=========TRAINING DIRECT QUAT MODEL==================')
         model_quat = PointNet(dim_out=4, normalize_output=True).to(device=device, dtype=tensor_type)
         loss_fn = quat_squared_loss
+        train_data, test_data = None, None
         (train_stats_quat, test_stats_quat) = train_test_model(args, train_data, test_data, model_quat, loss_fn, rotmat_targets=False, tensorboard_output=False)
         del(model_quat)
 
@@ -75,6 +77,7 @@ def main():
         print('==============TRAINING A (16 sym quat) MODEL====================')
         model_A_sym = QuatNet(enforce_psd=False, unit_frob_norm=True).to(device=device, dtype=tensor_type)
         loss_fn = quat_squared_loss
+        train_data, test_data = None, None
         (train_stats_A_sym, test_stats_A_sym) = train_test_model(args, train_data, test_data, model_A_sym, loss_fn,  rotmat_targets=False, tensorboard_output=False)
         del(model_A_sym)
 
@@ -82,12 +85,14 @@ def main():
         print('==============TRAINING A (16 psd quat) MODEL====================')
         model_A_psd = QuatNet(enforce_psd=True, unit_frob_norm=True).to(device=device, dtype=tensor_type)
         loss_fn = quat_squared_loss
+        train_data, test_data = None, None
         (train_stats_A_psd, test_stats_A_psd) = train_test_model(args, train_data, test_data, model_A_psd, loss_fn,  rotmat_targets=False, tensorboard_output=False)
         del(model_A_psd)
 
         print('==============TRAINING A (55 psd rotmat) MODEL====================')
         model_A_rotmat = RotMatSDPNet(enforce_psd=False, unit_frob_norm=True).to(device=device, dtype=tensor_type)
         loss_fn = rotmat_frob_squared_norm_loss
+        train_data, test_data = None, None
         (train_stats_A_rotmat, test_stats_A_rotmat) = train_test_model(args, train_data, test_data, model_A_rotmat, loss_fn,  rotmat_targets=True, tensorboard_output=False)
         del(model_A_rotmat)
 
