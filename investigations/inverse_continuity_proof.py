8c7f4f6b2840f8dd56e4f22f02f00f62576f3705
Matthew Giamou <matthew.giamou@gmail.com>
2020-01-14 13:10:38 -0500

    Add outlier rate plot to wahba_eigenvalue_gap.py

--- investigations/inverse_continuity_proof.py
+++ investigations/inverse_continuity_proof.py
@@ -23,6 +23,9 @@ def omega_right(q):
 if __name__ == '__main__':
 
     q = sym.symbols('q0:4')
+    q = sym.Matrix(q)
+    q_inv = -q
+    q_inv[-1] = -q_inv[-1]
     ## Householder check
     # w = sym.Matrix(q)
     # w[0] = w[0] + sym.sqrt(q[0]**2 + q[1]**2 + q[2]**2 + q[3]**2)
@@ -46,7 +49,7 @@ if __name__ == '__main__':
     # c1_hat[0:3, 0] = c1
     e1_hat = sym.zeros(4, 1)
     e1_hat[0] = 1
-    c1_hat = omega_left(q)*omega_right(q)*e1_hat
+    c1_hat = omega_left(q)*omega_right(q_inv)*e1_hat
 
     M_l = omega_left(c1_hat)
     M_r = omega_right(e1_hat)
@@ -56,12 +59,11 @@ if __name__ == '__main__':
     # c2_hat[0:3, 0] = c2
     e2_hat = sym.zeros(4, 1)
     e2_hat[1] = 1
-    c2_hat = omega_left(q) * omega_right(q) * e2_hat
+    c2_hat = omega_left(q) * omega_right(q_inv) * e2_hat
 
     M_l2 = omega_left(c2_hat)
     M_r2 = omega_right(e2_hat)
 
     A1 = sym.eye(4) + M_l * M_r
     A2 = sym.eye(4) + M_l2*M_r2
-
 
