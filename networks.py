03d4636acb9596e7add304ac7d2d51cc38e5a0ba
Valentin Peretroukhin <valentinp@gmail.com>
2020-01-21 01:12:26 -0500

    Added FLA plots
    Former-commit-id: 5750a9e6402a2ecb08ae5b5ae5f1f0c061d4e100 [formerly 6d4b56e91962fececa5558518505e08af4da6684]
    Former-commit-id: 20af88bf2df04f39e83000a566f526926aa47b0f

--- networks.py
+++ networks.py
@@ -246,7 +246,33 @@ class QuatFlowNet(torch.nn.Module):
         q = self.qcqp_solver(A_vec)
         return q
 
+class QuatFlowResNet(torch.nn.Module):
+    def __init__(self, enforce_psd=True, unit_frob_norm=True):
+        super(QuatFlowNet, self).__init__()
+        self.A_net = CustomResNet(dim_out=10)
+        self.enforce_psd = enforce_psd
+        self.unit_frob_norm = unit_frob_norm
+        self.qcqp_solver = QuadQuatFastSolver.apply
+    
+    def output_A(self, x):
+        A_vec = self.A_net(x)
+        if self.enforce_psd:
+            A_vec = convert_Avec_to_Avec_psd(A_vec)
+        if self.unit_frob_norm:
+            A_vec = normalize_Avec(A_vec)
+        
+        return convert_Avec_to_A(A_vec)
 
+    def forward(self, x):
+        A_vec = self.A_net(x)
+
+        if self.enforce_psd:
+            A_vec = convert_Avec_to_Avec_psd(A_vec)
+        if self.unit_frob_norm:
+            A_vec = normalize_Avec(A_vec)
+        
+        q = self.qcqp_solver(A_vec)
+        return q
 
 def conv_unit(in_planes, out_planes, kernel_size=3, stride=2,padding=1, batchnorm=True):
         if batchnorm:
