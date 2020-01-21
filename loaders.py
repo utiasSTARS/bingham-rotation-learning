7839b6e1f7a2f483276202d4604717a1ad09f3cb
Valentin Peretroukhin <valentinp@gmail.com>
2020-01-20 19:21:30 -0500

    New fla limits
    Former-commit-id: f090f955ef92a176d68e1cf802af6c9e0da824ad [formerly 08a684681bd33b48967f22e1f9259074a6b8ad89]
    Former-commit-id: 3c07a03207dadb474367b46ced7465f8b12e8a53

--- loaders.py
+++ loaders.py
@@ -387,6 +387,16 @@ class FLADataset(tud.Dataset):
     def __len__(self):
         return len(self.image_filenames) - 1
 
+    def compute_flow(self, img1, img2):
+        #Convert back to W x H x C
+        np_img1 = img1.permute(1,2,0).numpy()
+        np_img2 = img2.permute(1,2,0).numpy()
+
+        flow_cv2 = cv2.calcOpticalFlowFarneback(np_img1, np_img2, None, 0.5, 3, 15, 3, 5, 1.2, 0)
+        flow_img = torch.from_numpy(flow_cv2).permute(2,0,1)
+
+        return flow_img
+
     def find_pose(self, timestamp):
          # Find closest pose given timestamp.
         pose_idx = np.argmin(np.abs(self.pose_timestamps - timestamp))
@@ -402,7 +412,8 @@ class FLADataset(tud.Dataset):
 
         image1 = Image.open(os.path.join(self.image_dir, "data", self.image_filenames[id1]))
         image2 = Image.open(os.path.join(self.image_dir, "data", self.image_filenames[id2]))
-        
+        flow_image = self.compute_flow(image1, image2)
+
         pose_idx1 = self.find_pose(self.image_timestamps[id1])
         pose_idx2 = self.find_pose(self.image_timestamps[id2])
 
@@ -419,5 +430,5 @@ class FLADataset(tud.Dataset):
         else:
             target = rotmat_to_quat(R, ordering='xyzw')
 
-
-        return torch.cat([image1, image2], dim=0), target
+        img_input = flow_image #torch.cat([image1, image2], dim=0)
+        return img_input, target
