bc3775a6accb500318c11495ce4141c1636d72a4
Valentin Peretroukhin <valentinp@gmail.com>
2020-01-20 21:56:59 -0500

    Changing FLA
    Former-commit-id: 3b62d95a24beec06a7b90a9055c556b25388fb78 [formerly 308fcdbbe2c451cb6c59f46e111accba9d2709c1]
    Former-commit-id: 888b49defc8509d71d36f6d51e6f7058488a976e

--- loaders.py
+++ loaders.py
@@ -423,7 +423,8 @@ class FLADataset(tud.Dataset):
             else:
                 id1 = idx - len(self.image_filenames) 
                 id2 = id1 + 2
-            
+
+        print('{} and {}'.format(id1, id2))
         image1 = Image.open(os.path.join(self.image_dir, "data", self.image_filenames[id1]))
         image2 = Image.open(os.path.join(self.image_dir, "data", self.image_filenames[id2]))
 
