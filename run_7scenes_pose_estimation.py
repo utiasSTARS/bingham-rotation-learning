a839557ce03873257ec931a7f07ab72963f9f43e
Valentin Peretroukhin <valentinp@gmail.com>
2020-01-20 20:44:53 -0500

    Saving models
    Former-commit-id: 27f53a7552476d566515f9c95d547b74339c8a8b [formerly 792e60686d71eaf3345c09356ce4fe1df18add85]
    Former-commit-id: 7065d8a26ecc54595ca6f19f8d47a0cc320ecc59

--- run_7scenes_pose_estimation.py
+++ run_7scenes_pose_estimation.py
@@ -46,7 +46,7 @@ def main():
     #Load datasets
     transform_train = transforms.Compose([
         transforms.Resize(256),
-        transforms.CenterCrop(224),
+        transforms.RandomCrop(224),
         transforms.ToTensor(),
         transforms.Normalize(mean=[0.485, 0.456, 0.406],
                              std=[0.229, 0.224, 0.225])
